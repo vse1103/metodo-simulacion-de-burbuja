8ebcf85725634537163c82a498300f9e43b85a7f
vse1103 <134744640+vse1103@users.noreply.github.com>
2023-05-26 15:14:49 -0700

    ejercicio propuesto

---
+++ app12/app1/app de ordenamiento/Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app_de_ordenamiento
+{
+    public partial class Form1 : Form
+    {
+
+     //<<<<<<< datos inicio
+
+
+        bool estado = false;
+
+        int[] Arreglos_De_Numeros;
+
+        Numeros Datos = new Numeros();
+
+        Button[] Arreglos;
+
+
+     //>>>>>>> datos final
+
+        public Form1()
+        {
+
+         //<<<<<<< llamdos inicio
+
+            InitializeComponent();
+            tabPage1.Paint += tabPage1_Paint;
+            Arreglos_De_Numeros = new int[0];
+            Ordenamiento_insercion(Arreglos_De_Numeros);
+
+         //>>>>>>> llamados final
+
+        }
+
+
+
+
+
+
+
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+
+            //<<<<<<< BtnAgregar inicio
+
+            try
+            {
+
+
+                int numero = Convert.ToInt32(txtNumero.Text);
+                Datos.Insertar_Dato(numero);
+                Arreglos_De_Numeros = Datos.Obtener_Arreglo();
+
+                Arreglos = Datos.Arreglo_Botones();
+
+
+            }
+            catch
+            {
+
+
+                MessageBox.Show("Solo se admiten numeros enteros");
+
+       
+
+            }
+
+            estado = true;
+            tabPage1.Refresh();
+
+            //<<<<<<< BtnAgregar final
+
+        }
+
+        private void BtnOrdenar_Click(object sender, EventArgs e)
+        {
+
+            ///>>>>>>> BtnOrdenar inicio
+
+            this.Cursor = Cursors.WaitCursor;
+
+            BtnOrdenar.Enabled = false;
+            txtNumero.Enabled = false;
+            BtnAgregar.Enabled = false;
+
+            Stopwatch inicio = new Stopwatch();
+            Stopwatch final = new Stopwatch();
+            inicio.Start();
+
+
+            BubbleSort(ref Arreglos_De_Numeros, ref Arreglos);
+
+            final.Stop();
+            TimeSpan tiempo = inicio.Elapsed;
+
+            MessageBox.Show($"El ordenamiento tardó {tiempo.TotalSeconds} segundos");
+
+            this.Cursor = Cursors.Default;
+
+            BtnOrdenar.Enabled = true;
+            txtNumero.Enabled = true;
+            BtnAgregar.Enabled = true;
+
+            //>>>>>>> BtnOrdenar final
+
+        }
+
+        private void tabPage1_Paint(object sender, PaintEventArgs e)
+        {
+            //>>>>>>> tapepage_paint inicio
+
+            if (estado)
+            {
+
+
+                Point xy = new Point(50, 70);
+
+                try
+                {
+
+                    Dibujar_Arreglo(ref Arreglos, xy, ref tabPage1);
+
+                }
+
+                catch
+                {
+                }
+
+                estado = false;
+
+
+              //>>>>>>> tapepage_paint final
+
+        
+            }
+
+        }
+
+
+        class Numeros
+        {
+            //>>>>>>> Clase_numeros inicio
+
+            private int Longitud;
+            private int[] Arreglo = new int[1];
+            private Button[] Arreglos_Botones = new Button[1];
+
+            public Numeros()
+            {
+
+
+                int a = 0;
+                Arreglo[0] = a;
+                Arreglos_Botones[0] = new Button();
+                Arreglos_Botones[0].Width = 40;
+                Arreglos_Botones[0].Height = 40;
+                Arreglos_Botones[0].BackColor = Color.GreenYellow;
+                Arreglos_Botones[0].Text = a.ToString();
+                Calcular_Longitud();
+
+
+            }
+
+            public void Calcular_Longitud()
+            {
+
+
+                Longitud = Arreglos_Botones.Length;
+
+
+            }
+
+            public int Obtener_Longitud()
+            {
+
+
+                return Longitud;
+
+
+            }
+
+            public int[] Obtener_Arreglo()
+            {
+
+
+                return Arreglo;
+
+
+            }
+
+
+
+            public void Insertar_Dato(int Dato)
+            {
+
+
+                Array.Resize<int>(ref Arreglo, Longitud + 1);
+                Arreglo[Longitud] = Dato;
+
+                Array.Resize<Button>(ref Arreglos_Botones, Longitud + 1);
+
+
+                Arreglos_Botones[Longitud] = new Button();
+                Arreglos_Botones[Longitud].Width = 50;
+                Arreglos_Botones[Longitud].Height = 50;
+                Arreglos_Botones[Longitud].BackColor = Color.GreenYellow;
+                Arreglos_Botones[Longitud].Text = Dato.ToString();
+                Calcular_Longitud();
+
+
+            }
+
+            public Button[] Arreglo_Botones()
+            {
+
+
+                return Arreglos_Botones;
+
+
+            }
+
+            //>>>>>>> Clase_numeros final
+
+        }
+
+
+
+
+
+        public void BubbleSort(ref int[] Arreglos, ref Button[] Arreglo_De_Numeros)
+        {
+
+            //>>>>>>> BubbleSort inicio
+
+            for (int i = 0; i < Arreglos.Length; i++)
+            {
+
+                for (int j = 0; j < Arreglos.Length - 1; j++)
+                {
+
+
+                    if (Arreglos[j] > Arreglos[j + 1])
+                    {
+
+
+                        int aux = Arreglos[j];
+                        Arreglos[j] = Arreglos[j + 1];
+                        Arreglos[j + 1] = aux;
+
+                        Intercambio(ref Arreglo_De_Numeros, j + 1, j);
+
+                    }
+
+
+                }
+
+            }
+
+            //>>>>>>> BubbleSort final
+
+        }
+
+        
+
+
+        public void Dibujar_Arreglo(ref Button[] Arreglo, Point xy, ref TabPage t)
+        {
+            //>>>>>>> Dibujar_Arreglo inicio
+
+            for (int i = 0; i < Arreglo.Length; i++)
+            {
+
+
+                Arreglo[i].Location = xy;
+                t.Controls.Add(Arreglos[i]);
+                xy += new Size(70, 0);
+
+
+            }
+
+            //>>>>>>> Dibujar_Arreglo final
+        }
+
+
+
+
+       
+
+        public void Intercambio(ref Button[] boton, int a, int b)
+        {
+
+            //>>>>>>> Intercambio inicio
+
+            string temp = boton[a].Text;
+
+            Point pa = boton[a].Location;
+
+            Point pb = boton[b].Location;
+
+            int diferencia = pa.X - pb.X;
+            int x = 10;
+            int y = 10;
+            int t = 70;
+
+            while (y != 70)
+            {
+
+
+                Thread.Sleep(t);
+                boton[a].Location += new Size(0, 10);
+                boton[b].Location += new Size(0, -10);
+                y += 10;
+
+
+            }
+
+            while (x != diferencia + 10)
+            {
+
+                Thread.Sleep(t);
+                boton[a].Location += new Size(-10, 0);
+                boton[b].Location += new Size(10, 0);
+                x += 10;
+
+            }
+
+            y = 0;
+
+            while (y != -60)
+            {
+
+                Thread.Sleep(t);
+                boton[a].Location += new Size(0, -10);
+                boton[b].Location += new Size(0, +10);
+                y -= 10;
+            }
+
+
+
+
+            boton[a].Text = boton[b].Text;
+            boton[b].Text = temp;
+            boton[b].Location = pb;
+            boton[a].Location = pa;
+            estado = true;
+            tabPage1.Refresh();
+
+            //>>>>>>> Intercambio final
+
+        }
+
+        static void Ordenamiento_insercion(int[] array)
+        {
+
+            //>>>>>>> Ordenamiento_insercion
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                int temp = array[i];
+                int j = i - 1;
+
+                while ((j >= 0) && (array[j] > temp))
+                {
+
+                    array[j + 1] = array[j];
+                    j--;
+
+                }
+
+                array[j + 1] = temp;
+
+            }
+
+            //>>>>>>> Ordenamiento_insercion
+
+        }
+
+
+    }
+}
---
+++ app12/app1/app de ordenamiento/app1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace app_de_ordenamiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.BtnOrdenar = new System.Windows.Forms.Button();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresar numeros";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(254, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Simulacion de ordenamiento";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 151);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 287);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AutoScroll = true;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 261);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Simulador";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // BtnOrdenar
+            // 
+            this.BtnOrdenar.Location = new System.Drawing.Point(366, 55);
+            this.BtnOrdenar.Name = "BtnOrdenar";
+            this.BtnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.BtnOrdenar.TabIndex = 5;
+            this.BtnOrdenar.Text = "Ordenar";
+            this.BtnOrdenar.UseVisualStyleBackColor = true;
+            this.BtnOrdenar.Click += new System.EventHandler(this.BtnOrdenar_Click);
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.Location = new System.Drawing.Point(257, 55);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.BtnAgregar.TabIndex = 6;
+            this.BtnAgregar.Text = "Agregar";
+            this.BtnAgregar.UseVisualStyleBackColor = true;
+            this.BtnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(107, 57);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.BtnAgregar);
+            this.Controls.Add(this.BtnOrdenar);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button BtnOrdenar;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
+
